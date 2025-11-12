c4a720a8ce4a3708d6e9d68a99d0513c39896c56
Alberto Donato <albertodonato@users.noreply.github.com>
2025-11-12 15:48:47 +0200

    fix: add missing permissions for the discovery role [ENG-6074] (#23)
    [ENG-6074](https://stacklet.atlassian.net/browse/ENG-6074)
    
    ### what
    
    add missing permissions for the discover role
    
    ### why
    
    some permissions were missing preventing syncing everything
    
    ### testing
    
    n/a
    
    ### docs
    
    n/a
    
    
    [ENG-6074]:
    https://stacklet.atlassian.net/browse/ENG-6074?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- policies.tf
+++ policies.tf
@@ -1,11 +1,15 @@
 data "aws_iam_policy_document" "describe_augments" {
   statement {
     actions = [
+      "glue:GetConnections",
       "identitystore:Describe*",
       "identitystore:List*",
+      "quicksight:ListDashboards",
       "quicksight:ListGroups",
       "quicksight:ListUsers",
       "support:Describe*",
+      "timestream-influxdb:ListDbClusters",
+      "timestream-influxdb:ListDbInstances",
     ]
     resources = ["*"]
   }
